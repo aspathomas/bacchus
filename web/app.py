fa6a7d5b907555035f7d3b9a9cc8d199734c8160
thomas_aspa <aspa.thomas@gmail.com>
2023-09-15 11:57:30 +0200

    Suite connection base

--- web/app.py
+++ web/app.py
@@ -1,12 +1,42 @@
 import os
 from flask import Flask
+# from service.userService import UserService
+from models.model import db
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
 
 #we define the route /
 @app.route('/')
 def welcome():
     # return a json
-    return 'hello'
+    print('test')
+    return 'hello world'
+
+# @app.route("/login", methods=['POST'])
+# def login():
+#     data  = {
+#         'email': str(request.form.get("email")),
+#         'mdp': str(request.form.get("mdp")),
+#     }
+#     print(data)
+#     UserServ = UserService()
+#     return UserServ.login(data)
+
+# @app.route("/user", methods=['POST'])
+# def insertUser():
+#     data  = {
+#         'nom': str(request.form.get("nom")),
+#         'prenom': str(request.form.get("prenom")),
+#         'email': str(request.form.get("email")),
+#         'mdp': str(request.form.get("mdp")),
+#     }
+#     UserServ = UserService()
+#     return UserServ.insertUser(data)
 
 if __name__ == '__main__':
     #define the localhost ip and the port that is going to be used
